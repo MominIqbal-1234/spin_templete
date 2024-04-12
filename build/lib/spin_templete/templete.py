f5b179480f1dccd1ab3724c4087ae650a4590174
Momin Iqbal <61788052+MominIqbal-1234@users.noreply.github.com>
2024-04-12 18:47:02 +0000

    remove waste comments

--- build/lib/spin_templete/templete.py
+++ build/lib/spin_templete/templete.py
@@ -1,3 +1,42 @@
+# import os
+# from spin_sdk.http import Response
+# from jinja2 import Template
+# import json
+
+# class Render:
+    
+#     def __init__(self,templete:str,static:str=None) -> None:
+        
+#         self.templete = templete
+#         self.static = static
+
+
+#     def render(self,htmlFileName:str,**args):
+        
+#         self.filePath:str = os.path.join(self.templete,htmlFileName)
+#         template = Template(self.loadFile())
+#         self.res = template.render(args)
+
+#         return self.response("text/html")
+    
+#     def response(self,type:str):
+#         return Response(200,{"content-type": type},bytes(f"{self.res}", "utf-8"))
+
+
+#     def loadFile(self):
+#         with open(self.filePath,'r') as file:
+#             return file.read()
+
+#     def loadJson(self,JsonFileName):
+#         self.filePath:str = os.path.join(self.templete,JsonFileName)
+#         self.res =  json.loads(self.loadFile)
+#         return self.response("text/json")
+
+#     def loadStatic(self,FileName):
+#         self.filePath:str = os.path.join(self.static,FileName)
+#         return self.filePath
+
+
 
 
 import os
